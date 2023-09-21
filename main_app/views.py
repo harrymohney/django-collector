af179e395b402c73148af4312fd53094608fb864
harryhmohney <harrison.mohney@gmail.com>
2023-09-20 18:25:31 -0700

    views commit

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+finches = [
+    {'name': 'Canary Finch', 'color': 'Yellow', 'size': 'Small'},
+    {'name': 'Bengalese Finch', 'color': 'Brown', 'size': 'Small'},
+    {'name': 'Diamond Firetail Finch', 'color': 'Multicolored', 'size': 'Small'},
+    {'name': 'Owl Finch', 'color': 'White and Gray', 'size': 'Small'},
+    {'name': 'Zebra Finch', 'color': 'Gray', 'size': 'Small'}
+]
+
+def all_finches(request):
+    return render(request, 'main_app/all_finches.html', {'finches': finches})

6a513e6cc75061249e97be86e3c0b90d17aa4a72
harryhmohney <harrison.mohney@gmail.com>
2023-09-22 09:44:42 -0700

    queryset create

--- main_app/models.py
+++ main_app/models.py
@@ -5,3 +5,6 @@ class Finch(models.Model):
   name = models.CharField(max_length=100)
   color = models.CharField(max_length=50)
   size = models.TextField(max_length=50)
+
+  def __str__(self):
+    return self.name

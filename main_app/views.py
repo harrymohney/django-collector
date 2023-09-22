ed43b2f9bed12d8ad2f4b86fd3b8dc20a941c34e
harryhmohney <harrison.mohney@gmail.com>
2023-09-22 10:19:51 -0700

    model import update

--- main_app/views.py
+++ main_app/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from .models import Finch
 
 finches = [
-    {'name': 'Canary Finch', 'color': 'Yellow', 'size': 'Small'},
-    {'name': 'Bengalese Finch', 'color': 'Brown', 'size': 'Small'},
-    {'name': 'Diamond Firetail Finch', 'color': 'Multicolored', 'size': 'Small'},
-    {'name': 'Owl Finch', 'color': 'White and Gray', 'size': 'Small'},
-    {'name': 'Zebra Finch', 'color': 'Gray', 'size': 'Small'}
+    # {'name': 'Canary Finch', 'color': 'Yellow', 'size': 'Small'},
+    # {'name': 'Bengalese Finch', 'color': 'Brown', 'size': 'Small'},
+    # {'name': 'Diamond Firetail Finch', 'color': 'Multicolored', 'size': 'Small'},
+    # {'name': 'Owl Finch', 'color': 'White and Gray', 'size': 'Small'},
+    # {'name': 'Zebra Finch', 'color': 'Gray', 'size': 'Small'}
 ]
 
 def home(request):
@@ -15,10 +15,10 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
-# def index(request):
-#     finches = Finch.objects.all()
-#     context = {'finches': finches}
-#     return render(request, 'finches/index.html', context)
-
 def finches_index(request):
-    return render(request, 'finches_index.html')
+    cats = Finch.objects.all()
+    return render(request, 'finches/index.html', 
+    { 
+        'finches': finches
+    }
+)
